7f522eac698a271e0b4b775a4d737c42b26aeb94
Felix Wippich <felix.wippich@me.com>
2020-06-16 16:51:40 +0200

    Import google fonts

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">CV</router-link> |
       <router-link to="/contact">Contact</router-link>
     </div>
     <router-view />
@@ -9,12 +9,21 @@
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:ital,wght@0,300;0,700;1,300&family=Raleway:ital,wght@0,400;0,700;1,400&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+  font-family: "Open Sans Condensed", sans-serif;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+h1,
+h2,
+h3,
+h4 {
+  font-family: "Raleway", sans-serif;
+  -webkit-font-smoothing: antialiased;
 }
 
 #nav {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="home">
-    <h1>This is me</h1>
+    <h1>Felix</h1>
     <img alt="Vue logo" src="../assets/logo.png" />
+    <Tech />
   </div>
 </template>
 
 <script>
+import Tech from "@/components/Tech.vue";
+
 export default {
   name: "Home",
-  components: {}
+  components: {
+    Tech
+  }
 };
 </script>
